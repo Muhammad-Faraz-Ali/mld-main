4c6ef93a100cafe77bedf0d6d9289b1d989bae40
muhammad faraz ali <muhammad.faraz@confiz.com>
2024-03-24 16:04:34 +0500

    Resolving style issue

--- src/components/Navbar/NavLink.js
+++ src/components/Navbar/NavLink.js
@@ -1,11 +1,11 @@
 import { HashLink } from "react-router-hash-link";
 
-function NavLink({ text, link, style }) {
-  const styles =
-    style || "px-4 font-extrabold text-gray-500 hover:text-blue-900";
+function NavLink({ text, link, styles }) {
+  const style =
+    styles || "px-4 font-extrabold text-gray-500 hover:text-blue-900";
 
   return (
-    <HashLink className={styles} smooth to={link}>
+    <HashLink className={style} smooth to={link}>
       {text}
     </HashLink>
   );
